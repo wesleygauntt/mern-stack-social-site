be1bad2b6288e2d7c4f70de4b5049c665f1ce463
Wesley Gauntt <wesleygauntt92@gmail.com>
2019-04-15 20:47:03 -0700

    Added routes

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const auth = require("./routes/api/auth");
+const posts = require("./routes/api/posts");
+const profile = require("./routes/api/profile");
+
 const app = express();
 
 const db = require('./config/keys.js').mongoURI;
@@ -13,6 +17,9 @@ mongoose
   .catch(err => console.log("Error connecting to DB: ", err))
 
 app.get('/', (req, res) => res.send('Hit.'));
+app.use('/api/auth', auth);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 3000;
 
